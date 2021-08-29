e011bd3df08a98f56e0204d3d1331d2725f9c7f6
Roddy Bae <bsy17171@naver.com>
2021-08-30 00:05:23 +0900

    Protecting secret info

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const { User } = require("./models/User");
+const config = require("./config/key");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(
-    "mongodb+srv://bsy17171:bsy95989598@myfirst-back-end.hehnv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("MongoDB Connected..."))
   .catch((e) => console.log(e));
 mongoose.Promise = global.Promise;
 
-app.get("/", (req, res) => res.send("hello Wolrd_ FUCK CHINA"));
+app.get("/", (req, res) => res.send("hello Wolrd_ FUCK CHINA AND TANgasd"));
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
